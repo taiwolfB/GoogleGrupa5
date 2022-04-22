6a6842dbb0675d8d952296eb7062c6fd17634fc5
Bogdan Tailup <bogdan.tailup@mc3lab.com>
2022-04-22 12:37:15 +0300

    Previous weeks

---
+++ functiiW2C4.py
@@ -0,0 +1,35 @@
+cnp = '500131336381'
+
+S = cnp[0]
+AA = cnp[1:3]
+LL = cnp[3:5]
+ZZ = cnp[5:7]
+JJ = cnp[7:9]
+NNN = cnp[9:12]
+C = cnp[12]
+
+validation = '279146358279'
+
+
+def calculateValid():
+    result = 0
+    for i in range(12):
+        result += int(cnp[i]) * int(validation[i])
+    if result % 11 == 10:
+        return 1
+    return result % 11
+
+
+def isValidCnp(cnp):
+    if len(cnp) != 13:
+        return False
+    elif S == '0':
+        return False
+    elif NNN == '000':
+        return False
+    elif int(C) != calculateValid():
+        return False
+    return True
+
+
+print(isValidCnp(cnp))
